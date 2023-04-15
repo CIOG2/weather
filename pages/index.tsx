71aa42609973a9b6ee5c0b5f255e042da0d1645a
Carlos ivan <carlos.ivan10043@gmail.com>
2023-04-14 18:39:48 -0700

    Updated index to only render App component for the application

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@styles/Home.module.css'
+import { WeatherProvider } from "@context/WeatherContext";
+import { App } from "@components/APP";
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {
 
-export default function Home() {
   return (
-    <>
-    </>
+    <WeatherProvider>
+      <App />
+    </WeatherProvider>
   )
 }
+
+export default Home;

4694e3c14ac390f7b51cf36de48f3effd5be2961
Carlos ivan <carlos.ivan10043@gmail.com>
2023-04-14 23:22:14 -0700

    Added OptionCity to display recommended city in a better way.

---
+++ components/SearchCity/OptionCity/index.tsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import Styles from '@components/SearchCity/OptionCity/styles.module.scss';
+import { WeatherContext } from '@context/WeatherContext';
+
+const OptionCity = ({ mainText, description }: any) => {
+    const { mode } = useContext(WeatherContext);
+
+    return (
+        <div className={Styles['center']}>
+            <picture className={Styles['container__img']}>
+                <img 
+                    className={Styles['image']}
+                    src={`/svg/location-${mode}.svg`}
+                    alt="location icon"
+                />
+            </picture>
+            <div className={Styles['container__text']}>
+                <h4 
+                    className={`${Styles['title']} ${Styles[`title--${mode}`]}`}
+                > 
+                    {mainText} 
+                </h4>
+                <span 
+                    className={`${Styles['description']} ${Styles[`description--${mode}`]}`}
+                > 
+                    {description} 
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export { OptionCity }

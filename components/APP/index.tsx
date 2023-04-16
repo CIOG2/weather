8d4e560c5821c555fa316d048a152f40ff36e42a
Carlos ivan <carlos.ivan10043@gmail.com>
2023-04-16 13:36:22 -0700

    commit

--- components/APP/index.tsx
+++ components/APP/index.tsx
@@ -2,19 +2,32 @@ import { useContext } from 'react';
 import { Header } from '@components/Header';
 import { WeatherContext } from '@context/WeatherContext';
 import Styles from './styles.module.scss';
+import { WeatherCard } from '@components/WeatherCard';
+import { WeekWeatherCard } from '@components/WeekWeatherCard';
+import { Loader } from '@components/Loader';
 
 const App = () => {
-    const { mode } = useContext(WeatherContext);
+    const { mode, loading } = useContext(WeatherContext);
 
-    return (
-        <div
-            className={`${Styles['body']} ${Styles[`body--${mode}`]}`}
-        >
-            <Header />
-            <main>
-          
-            </main>
-      </div>
+        return (
+            <>
+            {loading 
+                ?(
+                    <div
+                        className={`${Styles['body']} ${Styles[`body--${mode}`]}`}
+                    >
+                        <Header />
+                        <main className={Styles['main']}>
+                            <aside className={Styles[`main__aside`]}>
+                                <WeatherCard />
+                                <WeekWeatherCard/>
+                            </aside>
+                        </main>
+                    </div>
+                )
+                :( <Loader />)
+            }
+        </>
     )
 }
 
--- context/WeatherContext/WeatherContext.tsx
+++ context/WeatherContext/WeatherContext.tsx
@@ -1,8 +1,16 @@
 import { createContext } from 'react';
+import { City } from '@interfaces/index';
 
 interface ContextProps {
     mode: 'dark' | 'light';
     setMode: (mode: 'dark' | 'light') => void;
+    city: {};
+    setCity: (obj: City ) => void;
+    loading: boolean;
+    weatherCity: any;
+    fetchWeatherCity: (obj: City) => void;
+    weatherWeek: any;
+    fetchWeatherWeek: (obj: City) => void;
 }
 
 const WeatherContext = createContext({} as ContextProps);

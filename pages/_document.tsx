58ee98c3d943262381e22d4f73b488a50c956735
Carlos ivan <carlos.ivan10043@gmail.com>
2023-04-16 13:26:21 -0700

    Add meta tag description to improve SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="es">
-      <Head />
+    <Html lang='en'>
+        <title>WeatherApp</title>
+      <Head>
+        <meta name="title" content="WeatherApp"/>
+        <meta name="description" content="Accurate weather forecasts for your day-to-day."/>
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://metatags.io/"/>
+        <meta property="og:title" content="WeatherApp"/>
+        <meta property="og:description" content="Accurate weather forecasts for your day-to-day."/>
+        <meta property="og:image" content="https://i.ibb.co/K9KqPqg/Weather.png"/>
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://metatags.io/"/>
+        <meta property="twitter:title" content="WeatherApp"/>
+        <meta property="twitter:description" content="Accurate weather forecasts for your day-to-day."/>
+        <meta property="twitter:image" content="https://i.ibb.co/K9KqPqg/Weather.png"/>
+
+      </Head>
       <body>
         <Main />
         <NextScript />

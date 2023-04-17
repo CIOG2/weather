abe186f75fac4fe61743b21ec6d9bb101e83c3a0
Carlos ivan <carlos.ivan10043@gmail.com>
2023-04-16 20:52:30 -0700

    add new issues

--- components/APP/index.tsx
+++ components/APP/index.tsx
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
+import { Loader } from '@components/Loader';
+import { Map } from '@components/Map';
 import { Header } from '@components/Header';
 import { WeatherContext } from '@context/WeatherContext';
-import Styles from './styles.module.scss';
 import { WeatherCard } from '@components/WeatherCard';
 import { WeekWeatherCard } from '@components/WeekWeatherCard';
-import { Loader } from '@components/Loader';
+import { WeatherDetails } from '@components/WeatherDetails';
+import Styles from './styles.module.scss';
 
 const App = () => {
     const { mode, loading } = useContext(WeatherContext);
@@ -16,12 +18,20 @@ const App = () => {
                     <div
                         className={`${Styles[`body`]} ${Styles[`body--${mode}`]}`}
                     >
-                        <Header />
+                        <Header/>
                         <main className={Styles[`main`]}>
                             <aside className={Styles[`main__aside`]}>
-                                <WeatherCard />
+                                <WeatherCard/>
                                 <WeekWeatherCard/>
                             </aside>
+                            <section className={Styles[`main__section`]}>
+                                <div className={Styles[`section__scroll`]}>
+                                    <div className={Styles[`container__map`]}>
+                                        <Map/> 
+                                    </div>
+                                    <WeatherDetails/>
+                                </div>
+                            </section>
                         </main>
                     </div>
                 )
